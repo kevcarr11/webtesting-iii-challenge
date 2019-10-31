6756ee2bf1be716d61a40bb510d8d0e445850445
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-30 20:14:26 -0500

    mvp met for testing Display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -10,7 +10,7 @@ test("Display render", () => {
 
 })
 
-test('Gate defaults to unlocked and open', () => {
+test('Displays if gate is open/closed and if it is locked/unlocked', () => {
   const wrapper = rtl.render(<Display />)
 
   const unlockedGate = wrapper.getByText(/unlocked/i)
@@ -19,4 +19,64 @@ test('Gate defaults to unlocked and open', () => {
   expect(unlockedGate).toBeDefined()
   expect(openGate).toBeDefined()
 })
+
+test('displays closed if the closed prop is true and open if otherwise', () => {
+  let toggled = true
+
+  const toggler = () => {
+    toggled = !toggled
+  }
+
+  const wrapper = rtl.render(<Display closed={toggled} />)
+
+  const closed = wrapper.getByText(/closed/i)
+  expect(closed).toBeDefined()
+
+  toggler()
+
+  const open = wrapper.findByText(/open/i)
+  expect(open).toBeDefined()
+})
+
+test('displays locked if the locked prop is true and open if otherwise', () => {
+  let toggled = true
+
+  const toggler = () => {
+    toggled = !toggled
+  }
+
+  const wrapper = rtl.render(<Display locked={toggled} />)
+  
+  const locked = wrapper.getByText(/locked/i)
+  expect(locked).toBeDefined()
+
+  toggler()
+
+  const unlocked = wrapper.findByText(/unlocked/i)  
+  expect(unlocked).toBeDefined();
+
+})
+
+test('when locked or closed use the red-led class', () => {
+  const wrapper = rtl.render(<Display locked={true} closed={true}/>)
+
+const locked = wrapper.queryAllByText('.red-led')
+expect(locked).toBeDefined()
+
+
+const closed = wrapper.queryAllByText('.red-led')
+expect(closed).toBeDefined();
+})
+
+test('when unlocked or open use the green-led class', () => {
+  const wrapper = rtl.render(<Display locked={false} closed={false}/>)
+
+const unlocked = wrapper.queryAllByText('.green-led')
+expect(unlocked).toBeDefined()
+
+
+const open = wrapper.queryAllByText('.green-led')
+expect(open).toBeDefined();
+})
+
 
