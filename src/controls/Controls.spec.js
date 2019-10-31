4734a9e4d68398ea9776988d4ee43cbddd8c07cb
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-30 19:37:44 -0500

    wrote simple render test for each component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,42 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+import { queryByText } from "react-testing-library";
+
+
+afterEach(rtl.cleanup)
+
+
+test("Controls render", () => {
+  rtl.render(<Controls />)
+})
+
+test("Open and Unlock", async () => {
+  const wrapper = rtl.render(<Controls />);
+
+  const lockButton = wrapper.queryByText("Lock Gate");
+  expect(lockButton.disabled).toBe(true)
+
+  const closeButton = wrapper.getByText("Close Gate")
+  expect(closeButton.disabled).toBe(false)
+
+ 
+  // rtl.act(() => {
+  //   rtl.fireEvent.click(open)
+  // })
+
+  // expect(mock).toBeCalled()
+})
+
+// describe("<Controls />", () => {
+//   it("open and unlock", () => {
+//     const mock = jest.fn();
+//     const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={mock} />)
+//     const lockButton = queryByText("Lock Gate");
+//     expect(lockButton.disabled).toBe(true)
+//     const closeButton = queryByText("Close Gate");
+//     expect(closeButton.disabled).toBe(false)
+//     fireEvent.click(closeButton);
+//     expect(mock).toBeCalled()
+//   })
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,22 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+afterEach(rtl.cleanup)
+
+test("Display render", () => {
+  rtl.render(<Display />)
+
+})
+
+test('Gate defaults to unlocked and open', () => {
+  const wrapper = rtl.render(<Display />)
+
+  const unlockedGate = wrapper.getByText(/unlocked/i)
+  const openGate = wrapper.getByText(/open/i)
+
+  expect(unlockedGate).toBeDefined()
+  expect(openGate).toBeDefined()
+})
+
