a4d425655926a06417176a1cb9be912b31c600cd
Your Name <kevincarr@Kevins-MacBook-Pro.local>
2019-10-30 21:43:01 -0500

    wrote tests for controls component mvp met all test pass

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,7 +2,6 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Controls from "./Controls";
-import { queryByText } from "react-testing-library";
 
 
 afterEach(rtl.cleanup)
@@ -12,31 +11,75 @@ test("Controls render", () => {
   rtl.render(<Controls />)
 })
 
-test("Open and Unlock", async () => {
-  const wrapper = rtl.render(<Controls />);
+test("Gate cannot be open or closed if it is locked", () => {
+  const toggleLocked = jest.fn()
 
-  const lockButton = wrapper.queryByText("Lock Gate");
-  expect(lockButton.disabled).toBe(true)
+  const wrapper = rtl.render(
+    <Controls locked={true} closed={true} toggleLocked={toggleLocked} />
+    )
 
-  const closeButton = wrapper.getByText("Close Gate")
-  expect(closeButton.disabled).toBe(false)
+  const openButton = wrapper.getByText(/open gate/i)
 
- 
-  // rtl.act(() => {
-  //   rtl.fireEvent.click(open)
-  // })
+  rtl.act(() => {
+    rtl.fireEvent.click(openButton)
+  })
 
-  // expect(mock).toBeCalled()
+  expect(toggleLocked).not.toHaveBeenCalled() 
+  
 })
 
-// describe("<Controls />", () => {
-//   it("open and unlock", () => {
-//     const mock = jest.fn();
-//     const { queryByText } = render(<Controls locked={false} closed={false} toggleClosed={mock} />)
-//     const lockButton = queryByText("Lock Gate");
-//     expect(lockButton.disabled).toBe(true)
-//     const closeButton = queryByText("Close Gate");
-//     expect(closeButton.disabled).toBe(false)
-//     fireEvent.click(closeButton);
-//     expect(mock).toBeCalled()
-//   })
+test('provide buttons to toggle the closed and locked states', () => {
+  const wrapper = rtl.render(<Controls />)
+
+  const closeButton = wrapper.getByText(/close gate/i)
+  const lockButton = wrapper.getByText(/lock gate/i)
+
+  expect(closeButton).toBeDefined()
+  expect(lockButton).toBeDefined()
+})
+
+test('buttons text changes to reflect the state the door will be in if clicked', () => {
+  const toggleClosed = jest.fn()
+
+  const wrapper = rtl.render(<Controls locked={false} closed={false} toggleClosed={toggleClosed} />)
+
+  const closeButton = wrapper.getByText(/close gate/i)
+
+  rtl.act(() => {
+    rtl.fireEvent.click(closeButton)
+  })
+
+  const openButton = wrapper.findByText(/open gate/i)
+
+  expect(closeButton).toBeDefined()
+  expect(openButton).toBeDefined()
+})
+
+test('the closed toggle button is disabled if the gate is locked', () => {
+  const toggleCloseButton = jest.fn()
+
+  const wrapper = rtl.render(<Controls locked={true} closed={true} toggleClosed={toggleCloseButton} />)
+
+  const openButton = wrapper.getByText(/open gate/i)
+
+  rtl.act(() => {
+    rtl.fireEvent.click(openButton)
+})
+  expect(toggleCloseButton).not.toBeCalled()
+})
+
+test('the locked toggle button is disabled if the gate is open', () => {
+  const toggleLockButton = jest.fn()
+
+  const wrapper = rtl.render(<Controls locked={true} closed={true} toggleClosed={toggleLockButton} />)
+
+  const lockButton = wrapper.getByText(/lock gate/i)
+
+  rtl.act(() => {
+    rtl.fireEvent.click(lockButton)
+})
+  expect(toggleLockButton).not.toBeCalled()
+
+})
+
+
